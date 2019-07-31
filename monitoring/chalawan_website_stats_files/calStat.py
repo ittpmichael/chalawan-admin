876bea22c75533cd24b6f2c62feb3ac01d49378e
ittpmichael <ittp.michael@gmail.com>
2019-07-31 18:11:36 +0700

    update calStat file

--- monitoring/chalawan_website_stats_files/calStat.py
+++ monitoring/chalawan_website_stats_files/calStat.py
@@ -4,34 +4,34 @@ import pandas as pd
 import numpy as np
 import datetime as dt
 
-D_TRACE=30
-
 #Filenames to read and write
 F_SENT="/home/ittipat/Documents/testlog/sentJobs.log"
 F_WALLCOM="/home/ittipat/Documents/testlog/walltimeComplete.log"
 F_USER="/home/ittipat/Documents/testlog/userRuntime.csv"
 
-#Read files and remove NaN column
+# Read data from file generated from 'qacct -o' 
+# which generate statistic of total running time for all processes type.
+# Remove NaN row
 df_wallcom = pd.read_table(F_WALLCOM,delim_whitespace=True,header=2)
 df_wallcom = df_wallcom[1:]
 
+# Read data from file generated from 'qstat -u "*"'.
 df_wallrun = pd.read_table(F_SENT,delim_whitespace=True,header=2)
 df_wallrun = df_wallrun[1:]
+# Drop state != 'r'
+df_wallrun[df_wallrun.state == 'r']
 
-#Connecting time strings
-df_wallrun['time']=df_wallrun['submit/start'].map(str)+" "+ df_wallrun['at'].map(str)
-df_wallrun['time']=pd.to_datetime(df_wallrun['time'])
+# Connecting time strings
+df_wallrun['time_start']=df_wallrun['submit/start'].map(str)+" "+ df_wallrun['at'].map(str)
+df_wallrun['time_start']=pd.to_datetime(df_wallrun['time_start'])
 
-#Convert to new dtype
-date_w = dt.datetime.now()-dt.timedelta(days=D_TRACE)
-sec_w = dt.timedelta(days=D_TRACE).total_seconds()
+# calculate time since submit
+df_wallrun['time_til_now']=(dt.datetime.now()-df_wallrun['time_start'])/dt.timedelta(seconds=int(1))
 
-df_wallrun['time_delta']=[sec_w if x<=date_w else (dt.datetime.now()-x)/dt.timedelta(seconds=1) for x in df_wallrun['time']]
-wall_time=np.sum(df_wallrun['time_delta'])
+# calculate CPU seconds for each user
+df_wallrun['cpus_secs']=df_wallrun['slots']*df_wallrun['time_til_now']
 
-df_wallrun['cpus_secs']=df_wallrun['slots']*df_wallrun['time_delta']
-
-dfuser=df_wallrun.groupby(['user']).sum()
+dfuser = df_wallrun.groupby(['user']).sum()
 dfuser = dfuser.reset_index()
 
 for i in range(len(dfuser)):
